50df7cadb6fb8f8241b2f6d0bc257fd3b566e21d
vamsidhar <vamsireddy617@gmail.com>
2020-02-21 14:44:45 -0500

    Created Basic Setup --Vamsi

---
+++ src/main/java/Student.java
@@ -0,0 +1,14 @@
+/**
+ * Created By ReddyGadu
+ * Created On 2/21/20
+ * Project Name Queueing-System-CSE542
+ **/
+public class Student {
+    String Name;
+    String email;
+    String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
